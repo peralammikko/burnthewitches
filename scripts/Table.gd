0ca7738939c573b00eb15fb5f7529596c46b68f8
peralammikko <mikko.perala@tuni.fi>
2024-03-19 18:49:46 +0200

    Fix results screen count

--- scripts/Table.gd
+++ scripts/Table.gd
@@ -24,7 +24,8 @@ func _ready():
 func _process(delta):
 	if customer_count > 5:
 		end_screen.show()
-		end_screen.display_stats(bad_witches_killed, good_witches_saved)
+		var goodies_saved = good_witches_saved + villagers_saved
+		end_screen.display_stats(bad_witches_killed, goodies_saved)
 
 # When customer is saved, adds value to "this-customer-type saved" variable for end screen.
 func _on_pass_button_pressed():
