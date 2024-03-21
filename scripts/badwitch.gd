d0eb9bdb3e717adbaf8f72bebad631bc802a1cdf
peralammikko <mikko.perala@tuni.fi>
2024-03-22 00:06:09 +0200

    Add suspect3 dialogue

--- scripts/badwitch.gd
+++ scripts/badwitch.gd
@@ -4,6 +4,7 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$DialogueBox.hide()
 	Dialogic.Save.delete_slot("Default")
 	var texture_index = randi_range(1,3)
 	match texture_index:
@@ -14,7 +15,8 @@ func _ready():
 		3:
 			$Sprite2D.texture = load("res://assets/customers/Tw3_journal_weavess.webp")
 			
-	$DialogueBox.create_new_dialogue(badwitchdialogue)
+	#$DialogueBox.create_new_dialogue(badwitchdialogue)
+	Dialogic.start("suspect_3")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
