07fe6543ab81237991312035390bb268990ae5a8
Mikko Perälä <mikko.perala@tuni.fi>
2024-03-22 15:40:55 +0200

    Camera scrolling with mouse wheel

--- scenes/Camera2D.gd
+++ scenes/Camera2D.gd
@@ -3,6 +3,7 @@ extends Camera2D
 @onready var low_camera = get_node("../lowerCamPos")
 
 var upper_position: Vector2
+var camera_up = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,9 +14,9 @@ func _ready():
 # If player pushes 'down' or 's', camera moves down.
 # If player pushes 'up' or 'w', camera moves up.
 func _process(delta):
-	if Input.is_action_just_pressed("down"):
+	if Input.is_action_just_pressed("scrolldown"):
 		_on_camera_down_button_pressed()
-	elif Input.is_action_just_pressed("up"):
+	elif Input.is_action_just_pressed("scrollup"):
 		_on_camera_up_button_pressed()
 
 # Smooth camera scrolling down
@@ -24,9 +25,11 @@ func _on_camera_down_button_pressed():
 	tween.tween_property(self, "position", low_camera.global_position, 0.2).set_ease(Tween.EASE_OUT)
 	$Camera_down_button.hide()
 	$Camera_up_button.show()
-	if Dialogic.Text.is_textbox_visible():
+	if camera_up:
 		Dialogic.Save.save("",false,Dialogic.Save.ThumbnailMode.NONE)
 		Dialogic.end_timeline()
+		camera_up = false
+		#Dialogic.Text.hide_textbox()
 
 # Smooth camera scrolling up
 func _on_camera_up_button_pressed():
@@ -34,7 +37,8 @@ func _on_camera_up_button_pressed():
 	tween.tween_property(self, "position", upper_position, 0.2).set_ease(Tween.EASE_OUT)
 	$Camera_up_button.hide()
 	$Camera_down_button.show()
-	if Dialogic.Save.has_slot("Default"):
+	if !camera_up:
 		Dialogic.Save.load()
+		camera_up = true
 	
 	
