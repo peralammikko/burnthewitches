ed74e7feb8629c4c7812970f5d22447e45e863fc
peralammikko <mikko.perala@tuni.fi>
2024-04-04 13:53:44 +0300

    Add incense burner and mood-changing flame

---
+++ scenes/MoodDetector.gd
@@ -0,0 +1,18 @@
+extends Sprite2D
+
+@onready var mood_calm = load("res://styles/moodflames/mood_calm.tres")
+@onready var mood_angry = load("res://styles/moodflames/mood_angry.tres")
+@onready var flameColor = $MoodFlame.process_material.color_ramp.gradient
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Dialogic.signal_event.connect(_on_dialogic_signal)
+	setFlameColor(mood_calm)
+
+func _on_dialogic_signal(argument: String):
+	if argument == "mood_angry":
+		print("Switch to angery")
+		setFlameColor(mood_angry)
+
+func setFlameColor(newMood):
+	$MoodFlame.process_material.set("color_ramp", newMood)
