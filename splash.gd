dc2743ffe29727af6df125f24ec9ffcd2b36d943
peralammikko <mikko.perala@tuni.fi>
2024-04-11 17:16:48 +0300

    Fix splashscreen black overlay bug

--- splash.gd
+++ splash.gd
@@ -13,4 +13,8 @@ func _on_jingle_timer_timeout():
 	AudioManager.intro_jingle.play()
 	
 func _on_game_start_timer_timeout():
-	get_tree().change_scene_to_file(mainmenu)
+	$AnimationPlayer.play("TransitionOut")
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "TransitionOut":
+		get_tree().change_scene_to_file(mainmenu)
